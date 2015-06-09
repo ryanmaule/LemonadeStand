0262ce04867087a7e5f81e6055f8390f9e4a6ac7
ryan <ryan@RYPLM019-Ryan-Home.local>
2015-06-08 22:56:26 -0400

    First version complete

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,17 +9,233 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let kStartingFunds = 10
+    let kStartingLemons = 1
+    let kStartingIceCubes = 1
+    let kLemonPrice = 2
+    let kIceCubePrice = 1
+    
+    var totalBalance = 0
+    var totalLemons = 0
+    var totalIceCubes = 0
+    
+    var buyingLemons = 0
+    var buyingIceCubes = 0
+    
+    var usingLemons = 0
+    var usingIceCubes = 0
+    
+    @IBOutlet weak var labelDollars: UILabel!
+    @IBOutlet weak var labelLemons: UILabel!
+    @IBOutlet weak var labelIceCubes: UILabel!
+    @IBOutlet weak var labelBuyLemonsQuantity: UILabel!
+    @IBOutlet weak var labelBuyIceCubesQuantity: UILabel!
+    @IBOutlet weak var labelLemonsPrice: UILabel!
+    @IBOutlet weak var labelIceCubesPrice: UILabel!
+    @IBOutlet weak var labelUseLemonsQuantity: UILabel!
+    @IBOutlet weak var labelUseIceCubesQuantity: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupDisplay()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupDisplay() {
+        self.labelLemonsPrice.text = "$\(self.kLemonPrice)"
+        self.labelIceCubesPrice.text = "$\(self.kIceCubePrice)"
+        self.totalBalance = self.kStartingFunds
+        self.totalLemons = self.kStartingLemons
+        self.totalIceCubes = self.kStartingIceCubes
+        self.updateDisplay()
+    }
+    
+    func updateDisplay() {
+        self.labelDollars.text = "$\(self.totalBalance) Dollar" + (outputPlural(self.totalBalance))
+        self.labelLemons.text = "\(self.totalLemons) Lemon" + (outputPlural(self.totalLemons))
+        self.labelIceCubes.text = "\(self.totalIceCubes) Ice Cube" + (outputPlural(self.totalIceCubes))
+        self.labelBuyLemonsQuantity.text = "\(self.buyingLemons)"
+        self.labelBuyIceCubesQuantity.text = "\(self.buyingIceCubes)"
+        self.labelUseLemonsQuantity.text = "\(self.usingLemons)"
+        self.labelUseIceCubesQuantity.text = "\(self.usingIceCubes)"
+    }
+    
+    func resetDay() {
+        self.buyingLemons = 0
+        self.buyingIceCubes = 0
+        self.usingLemons = 0
+        self.usingIceCubes = 0
+        self.updateDisplay()
+        
+        if self.totalLemons == 0 && self.totalBalance < kLemonPrice {
+            showAlertWithText(message: "Game Over: Can't afford Lemons")
+        }
+        else if self.totalIceCubes == 0 && self.totalBalance < kIceCubePrice {
+            showAlertWithText(message: "Game Over: Can't afford Ice Cubes")
+        }
+    }
+    
+    func outputPlural(myAmount: Int) -> String {
+        if myAmount > 1 || myAmount == 0 {
+            return "s"
+        }
+        else {
+            return ""
+        }
+    }
+    
+    func showAlertWithText(header: String = "Warning", message: String) {
+        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func addBuyLemons(sender: UIButton) {
+        if self.totalBalance >= self.kLemonPrice {
+            self.totalBalance -= self.kLemonPrice
+            self.buyingLemons++
+            self.totalLemons++
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "Not enough funds to buy Lemons")
+        }
+    }
+    
+    @IBAction func subBuyLemons(sender: UIButton) {
+        if self.totalLemons > 0 {
+            self.totalBalance += self.kLemonPrice
+            self.buyingLemons--
+            self.totalLemons--
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're out of Lemons")
+        }
+    }
+    
+    @IBAction func addBuyIceCubes(sender: UIButton) {
+        if self.totalBalance >= self.kIceCubePrice {
+            self.totalBalance -= self.kIceCubePrice
+            self.buyingIceCubes++
+            self.totalIceCubes++
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "Not enough funds to buy Ice Cubes")
+        }
+    }
+    
+    @IBAction func subBuyIceCubes(sender: UIButton) {
+        if self.totalIceCubes > 0 {
+            self.totalBalance += self.kIceCubePrice
+            self.buyingIceCubes--
+            self.totalIceCubes--
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're out of Ice Cubes")
+        }
+    }
+    
+    @IBAction func addUseLemons(sender: UIButton) {
+        if self.totalLemons > 0 {
+            self.totalLemons--
+            self.usingLemons++
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're out of Lemons")
+        }
+    }
+    
+    @IBAction func subUseLemons(sender: UIButton) {
+        if self.usingLemons > 0 {
+            self.totalLemons++
+            self.usingLemons--
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're not using any Lemons")
+        }
+    }
+    
+    @IBAction func addUseIceCubes(sender: UIButton) {
+        if self.totalIceCubes > 0 {
+            self.totalIceCubes--
+            self.usingIceCubes++
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're out of Ice Cubes")
+        }
+    }
+    
+    @IBAction func subUseIceCubes(sender: UIButton) {
+        if self.usingIceCubes > 0 {
+            self.totalIceCubes++
+            self.usingIceCubes--
+            self.updateDisplay()
+        }
+        else {
+            showAlertWithText(message: "You're not using any Ice Cubes")
+        }
+    }
+    
+    @IBAction func startDay(sender: UIButton) {
+        if self.usingLemons > 0 && self.usingIceCubes > 0 {
+            var totalCustomers = Int(arc4random_uniform(UInt32(10)))
+            println("Total Customers: \(totalCustomers)")
+            var recipeRatio = Double(usingLemons) / Double(usingIceCubes)
+        
+            for var customer = 0; customer < totalCustomers; customer++ {
+                var customerPreference = 1.0/(Double(Int(arc4random_uniform(UInt32(10))))+1.0)
+                println("Customer Preference: \(customerPreference)")
+                println("Recipe Ratio: \(recipeRatio)")
+                if customerPreference >= 0 && customerPreference <= 0.4 {
+                    if recipeRatio > 1 {
+                        println("Paid: Loves Acidic!")
+                        totalBalance++
+                    }
+                    else {
+                        println("No Match: Loves Acidic")
+                    }
+                }
+                else if customerPreference > 0.4 && customerPreference <= 0.6 {
+                    if recipeRatio == 1 {
+                        println("Paid: Loves Equal Parts!")
+                        totalBalance++
+                    }
+                    else {
+                        println("No Match: Loves Equal Parts")
+                    }
+                }
+                else if customerPreference > 0.6 && customerPreference <= 1.0 {
+                    if recipeRatio < 1 {
+                        println("Paid: Loves Diluted!")
+                        totalBalance++
+                    }
+                    else {
+                        println("No Match: Loves Diluted")
+                    }
+                }
+                else {
+                    println("Error Occurred: Can't Prefer > 1")
+                }
+            }
+            self.resetDay()
+        }
+        else {
+            showAlertWithText(message: "You must use both Lemons and Ice Cubes")
+        }
+    }
 
 }
 
